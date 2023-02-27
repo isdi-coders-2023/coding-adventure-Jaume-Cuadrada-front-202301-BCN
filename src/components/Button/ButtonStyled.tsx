9f1c6af43a43fb0da6710c7a809799db71a62963
jaumecuadrada <jaumecuadrada.audio@gmail.com>
2023-02-27 16:55:00 +0100

    Add button component styles

---
+++ src/components/Button/ButtonStyled.tsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const ButtonStyled = styled.button`
+  font-family: "Saira Stencil One";
+  font-weight: 400px;
+  font-size: 20px;
+  border-radius: 10px;
+  background-color: #cccf18;
+  color: #1e1e1e;
+  width: 323px;
+  height: 40px;
+`;
+
+export default ButtonStyled;
