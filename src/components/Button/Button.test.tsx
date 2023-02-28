15a786f6c55cf36e746b2b7248fcbf84b1ced9ed
jaumecuadrada <jaumecuadrada.audio@gmail.com>
2023-02-28 16:11:23 +0100

    Remove duplicated variable on button test

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -4,11 +4,11 @@ import Button from "./Button";
 describe("Given a Button Component", () => {
   describe("When rendered with 'Sign up' text content", () => {
     test("Then it should show a button with 'Sign up' text on it", () => {
-      const buttonContent = "Sign up";
       const expectedContent = "Sign up";
 
-      render(<Button text={buttonContent} />);
+      render(<Button text={expectedContent} />);
       const button = screen.getByRole("button", { name: expectedContent });
+
       expect(button).toBeInTheDocument();
     });
   });
