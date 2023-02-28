f1978befba4ef2a8b72c57a5a2dc7e0476fdb590
jaumecuadrada <jaumecuadrada.audio@gmail.com>
2023-02-28 15:53:55 +0100

    Add form component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
+import Form from "./components/Form/Form";
 import Button from "./components/Button/Button";
 
 const App = (): JSX.Element => {
   return (
     <>
       <p>Hello Developers!</p>
+      <Form />
       <Button text="Sign up" />
     </>
   );
